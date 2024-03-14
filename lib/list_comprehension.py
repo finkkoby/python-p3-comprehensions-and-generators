d02e5a86268f4fb03f33c28f166235511aa9456f
Koby Fink <finkkoby@icloud.com>
2024-03-14 10:47:42 -0400

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    new_list = [n for n in num_list if (n % 2 == 0)==True]
+    return new_list
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [(f"{sentence}!") for sentence in sentence_list]
+    return new_list
